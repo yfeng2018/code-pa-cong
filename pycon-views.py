16baaba69fa1b5a9d7d2ac88f71c48dd688bf834
fgggg <10428912+yfeng2018@users.noreply.github.com>
2021-01-11 17:42:14 +0800

    Update pycon-views.py
    添加简单的中文描述

--- pycon-views.py
+++ pycon-views.py
@@ -7,13 +7,13 @@ import bs4
 root_url = 'http://pyvideo.org'
 index_url = root_url + '/category/50/pycon-us-2014'
 
-
+# 获得网页地址
 def get_video_page_urls():
     response = requests.get(index_url)
     soup = bs4.BeautifulSoup(response.text)
     return [a.attrs.get('href') for a in soup.select('div.video-summary-data a[href^=/video]')]
 
-
+# 获得视频信息
 def get_video_data(video_page_url):
     video_data = {}
     response = requests.get(root_url + video_page_url)
